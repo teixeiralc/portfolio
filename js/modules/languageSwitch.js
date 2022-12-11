21f653102626c13c67b92e1a49b4d5f9ccae47ec
Lucas Teixeira <lucascteixeira0@gmail.com>
2022-12-11 19:43:33 -0300

    fix website for javascript disabled and minor improvements

--- js/modules/languageSwitch.js
+++ js/modules/languageSwitch.js
@@ -1,14 +1,9 @@
 export default function languageSwitch() {
   const languageSelect = document.querySelector('[data-language]');
   const URLs = {
-    ptBR: '/portfolio/',
-    enUK: `/portfolio/en`,
+    pt: '/portfolio/',
+    en: `/portfolio/en`,
   };
-
-  if (window.location.pathname === '/projetos.html') {
-    URLs.ptBR = '/portfolio/projetos.html';
-    URLs.enUK = '/portfolio/projetos-en.html';
-  }
 
   const redirect = () => {
     // On change, will redirect the user to the desired language page
