34150adbb8cd24a2f7cc49279e96364eaf6c6935
Lucas Teixeira <lucascteixeira0@gmail.com>
2023-06-21 15:40:13 -0300

    add: typescript to the typeEffect animation

--- js/modules/typeEffect.js
+++ js/modules/typeEffect.js
@@ -1,51 +1,36 @@
 export default function typeEffect() {
-  // List of sentences
-  const content = ['JavaScript', 'React', 'CSS', 'Figma', 'UI Design'];
+  const content = ['JavaScript', 'React', 'TypeScript', 'Ui Design', 'Figma'];
 
-  // Current sentence being processed
   let part = 0;
-
-  // Character number of the current sentence being processed
   let partIndex = 0;
-
-  // Holds the handle returned from setInterval
   let intertvalVal;
 
-  // Element that holds the text
   const element = document.querySelector('.type_effect_text');
 
-  // Cursor element
   const cursor = document.querySelector('.cursor');
 
-  // Implements typing effect
   if (element) {
 
     // eslint-disable-next-line no-inner-declarations
     function Type() {
-      // Get substring with 1 characater added
       const text = content[part].substring(0, partIndex + 1);
       element.innerHTML = text;
       partIndex++;
   
-      // Implements deleting effect
       function Delete() {
-        // Get substring with 1 characater deleted
         // eslint-disable-next-line no-shadow
         const text = content[part].substring(0, partIndex - 1);
         element.innerHTML = text;
         partIndex--;
   
-        // If sentence has been deleted then start to display the next sentence
         if (text === '') {
           clearInterval(intertvalVal);
   
-          // If current sentence was last then display the first one, else move to the next
           if (part === content.length - 1) part = 0;
           else part++;
   
           partIndex = 0;
   
-          // Start to display the next sentence after some time
           setTimeout(() => {
             cursor.style.display = 'inline-block';
             intertvalVal = setInterval(Type, 100);
@@ -53,9 +38,7 @@ export default function typeEffect() {
         }
       }
   
-      // If full sentence has been displayed then start to delete the sentence after some time
       if (text === content[part]) {
-        // Hide the cursor
         cursor.style.display = 'none';
   
         clearInterval(intertvalVal);
@@ -65,7 +48,6 @@ export default function typeEffect() {
       }
     }
   
-    // Start the typing effect on load
     intertvalVal = setInterval(Type, 100);
   }
 }
